e01f395ce35929d9b6b90e2de0a546166854993d
jm choi <527wlals@gmail.com>
2021-03-14 17:34:40 +0900

    전체적으로 user 모델관련 부분 수정

--- vote/views.py
+++ vote/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 from vote.models import *
 from .forms import UserForm
 from .models import User, Candidate
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 
 
 def home(request):
@@ -35,10 +37,13 @@ def user_info(request):
         form = UserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            # result = Result.objects.create()
-            # user.result = result
-            # user.save()
-            # return render(request, "vote/test.html")
+            user.username = user.id
+            user.save()
+
+            is_user = authenticate(request, username=user.username)
+            if is_user is not None:
+                auth_login(request, is_user)
+
             return redirect("vote:home")
         else:
             ctx = {
