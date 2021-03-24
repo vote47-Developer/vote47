47165f190c537de15c0eb6841d26caf7e4c3e127
Junan Kim <junankim18@naver.com>
2021-03-24 23:37:34 +0900

    result, detail 데이터 완성

--- vote/urls.py
+++ vote/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('vote', views.home, name='vote'),
     path('result', views.home, name='result'),
     path('candidate', views.candidate, name='candidate'),
+    path('detail', views.detail, name='detail'),
     path('answer', views.save_answer, name='save_answer'),
     path('api/get/quiz-list', views.get_quiz_list, name='get_quiz_list'),
     path("", views.user_info, name="user_info"),
