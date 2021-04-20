9f64e6048c7becc3a2e4c3dce7795aad61fa47b3
ingong <insong1216@gmail.com>
2021-04-20 20:50:00 +0900

    chore: 주석제거

--- config/static/javascript/index.js
+++ config/static/javascript/index.js
@@ -1,48 +1,28 @@
-const startPage = new StartPage()
-const quizPage = new QuizPage()
-const resultPage = new ResultPage()
+const startPage = new StartPage();
+const quizPage = new QuizPage();
+const resultPage = new ResultPage();
 
 function Router() {
-	this.route = async (path) => {
-		// history.state 가 false 이거나 
-		if (!history.state || path !== history.state.path)
-			history.pushState({ path }, '', path)
- 
-		if (path === '') {
-			startPage.init()
-			app.innerHTML = ''
-			app.appendChild(startPage.target)
-		} else if (path === 'quiz') {
-			await quizPage.init()
-			app.innerHTML = ''
-			app.appendChild(quizPage.target)
-		} else if (path === 'result') {
-			// const resultList = quizPage.getResultList()
-			// // 퀴즈의 개수가 0개라면 quiz 로 다시 route tlzl
-			// if (resultList.length === 0) {
-			// 	this.route('quiz')
-			// 	return
-			// }
-			// resultPage.init(resultList)
-			// app.innerHTML = ''
-			// app.appendChild(resultPage.target)
-			window.location = "http://127.0.0.1:8000/candidate";
-		}
-	}
+  this.route = async path => {
+    if (!history.state || path !== history.state.path) history.pushState({ path }, '', path);
+    if (path === '') {
+      startPage.init();
+      app.innerHTML = '';
+      app.appendChild(startPage.target);
+    } else if (path === 'quiz') {
+      await quizPage.init();
+      app.innerHTML = '';
+      app.appendChild(quizPage.target);
+    } else if (path === 'result') {
+      window.location = 'http://127.0.0.1:8000/candidate';
+    }
+  };
 }
 
-// router 생성
-// 생성자 함수에 의한 객체 생성
-const router = new Router()
-const app = document.querySelector('#app')
+const router = new Router();
+const app = document.querySelector('#app');
 
-router.route(location.pathname.replace('/', ''))
-// history.pushState 를 통하여 새 데이터 전달을 위한 상태, 제목, url 지정
-// ? window 의 객체의 popstate 이벤트를 이용
-// window 객체의 location 프로퍼티는 현재 창에 표시된 문서의 URL 을 나타내는 Location 객체와 연결
-// 이 객체에는 해당 창에 새 문서를 불러들이는 메서드도 존재한다.
-// replace(url) : 새로운 주소 이동(세션 히스토리가 남지 않기 때문에 back버튼으로 이동 불가) 
-
+router.route(location.pathname.replace('/', ''));
 window.addEventListener('popstate', () => {
-	router.route(location.pathname.replace('/', ''))
-})
+  router.route(location.pathname.replace('/', ''));
+});
--- config/static/javascript/pages/QuizPage.js
+++ config/static/javascript/pages/QuizPage.js
@@ -1,76 +1,77 @@
 function QuizPage() {
-	this.quizList = []
+  this.quizList = [];
 
-	this.init = async () => {
-		await this.loadQuizList()
-		this.setElements()
-		this.bindEvents()
-		this.setCurrentQuiz(this.quizList[0])
-	}
+  this.init = async () => {
+    await this.loadQuizList();
+    this.setElements();
+    this.bindEvents();
+    this.setCurrentQuiz(this.quizList[0]);
+  };
 
-	this.loadQuizList = async () => {
-		const apiUrl = getQuizListUrl()
+  this.loadQuizList = async () => {
+    const apiUrl = getQuizListUrl();
+    console.log(apiUrl);
 
-		const data = await (await fetch(apiUrl)).json()
-		this.quizList = data['quiz_list']
-	}
+    const data = await (await fetch(apiUrl)).json();
+    this.quizList = data['quiz_list'];
+  };
 
-	this.setElements = () => {
-		this.template = /*html*/ `
+  this.setElements = () => {
+    this.template = /*html*/ `
 			<div id="quiz-page">
 				<div class="quiz-container"></div>
 			</div>
-		`
-		this.target = document.createElement('div')
-		this.target.innerHTML = this.template
-		this.target = this.target.firstElementChild
+		`;
+    this.target = document.createElement('div');
+    this.target.innerHTML = this.template;
+    this.target = this.target.firstElementChild;
 
-		this.quizContainer = this.target.querySelector('.quiz-container')
+    this.quizContainer = this.target.querySelector('.quiz-container');
 
-		this.quizList = this.quizList.map((quizInfo) => {
-			const quiz = new Quiz(this, quizInfo)
-			this.quizContainer.appendChild(quiz.target)
+    this.quizList = this.quizList.map(quizInfo => {
+      const quiz = new Quiz(this, quizInfo);
+      this.quizContainer.appendChild(quiz.target);
 
-			return quiz
-		})
-	}
+      return quiz;
+    });
+  };
 
-	this.bindEvents = () => {}
+  this.bindEvents = () => {};
 
-	this.setCurrentQuiz = (newCurrentQuiz) => {
-		this.quizList.forEach((quiz) => quiz.hide())
-		newCurrentQuiz.show()
-	}
+  this.setCurrentQuiz = newCurrentQuiz => {
+    this.quizList.forEach(quiz => quiz.hide());
+    newCurrentQuiz.show();
+  };
 
-	this.selectAnswer = (answerInfo) => {
-		const currentQuiz = this.quizList.reduce(
-			(currentQuizInfo, quiz, index) => {
-				return quiz.id === answerInfo.quizId
-					? {
-							index,
-							quiz,
-					  }
-					: currentQuizInfo
-			},
-			{ index: 0, quiz: {} }
-		)
-		currentQuiz.quiz.answer = answerInfo.answerIndex
+  this.selectAnswer = answerInfo => {
+    const currentQuiz = this.quizList.reduce(
+      (currentQuizInfo, quiz, index) => {
+        return quiz.id === answerInfo.quizId
+          ? {
+              index,
+              quiz,
+            }
+          : currentQuizInfo;
+      },
+      { index: 0, quiz: {} }
+    );
+    currentQuiz.quiz.answer = answerInfo.answerIndex;
 
-		const nextQuiz = this.quizList[currentQuiz.index + 1]
-		setTimeout(() => {
-			if (nextQuiz) {
-				this.setCurrentQuiz(nextQuiz)
-			} else {
-				router.route('result')
-			}
-		}, 800)
-	}
+    const nextQuiz = this.quizList[currentQuiz.index + 1];
+    setTimeout(() => {
+      if (nextQuiz) {
+        this.setCurrentQuiz(nextQuiz);
+      } else {
+        router.route('result');
+      }
+    }, 800);
+  };
 
-	this.getResultList = () => {
-		return this.quizList.map((quiz) => ({
-			question: quiz.question,
-			examples: quiz.examples,
-			answer: quiz.answer,
-		}))
-	}
+  this.getResultList = () => {
+    return this.quizList.map(quiz => ({
+      question: quiz.question,
+      examples: quiz.examples,
+      answer: quiz.answer,
+    }));
+  };
 }
